361a80b920ab6e4a5a6d13d2e7032f11963d1758
Nikhil Kamdi <kamdinrao@gmail.com>
2023-03-16 17:24:51 +0530

    dfd

--- di.js
+++ di.js
@@ -14,27 +14,27 @@ function addItem(e){
   e.preventDefault();
 
   // Get input value
-  var newItem = document.getElementById('item').value;
-
+  var newItem1 = document.getElementById('item1').value;
+  let newItem2 = document.getElementById('item2').value;
   // Create new li element
   var li = document.createElement('li');
   // Add class
   li.className = 'list-group-item';
   // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
-
+  li.appendChild(document.createTextNode(newItem1));
+  li.appendChild(document.createTextNode(newItem2));
   // Create del button element
   var deleteBtn = document.createElement('button');
-
+  var editBtn = document.createElement('button');
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
+  editBtn.className = 'btn float-right';
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
-
+  editBtn.appendChild(document.createTextNode('Edit'));
   // Append button to li
   li.appendChild(deleteBtn);
-
+  li.appendChild(editBtn);
   // Append li to list
   itemList.appendChild(li);
 }
@@ -49,3 +49,21 @@ function removeItem(e){
   }
 }
 
+// Filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item1){
+      var item1Name = item1.firstChild.textContent;
+      
+      if(item1Name.toLowerCase().indexOf(text) != -1){
+        item1.style.display = 'block';
+        
+      } else {
+        item1.style.display = 'none';
+      }
+    });
+  }
